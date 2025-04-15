57bfeeb72ff675034af1dbc885b91b785abe2d2e
sabarim6369 <sabarim6369@gmail.com>
2025-04-15 20:58:48 +0530

    changes backend url

--- portfolio/src/components/Contact/Contact.jsx
+++ portfolio/src/components/Contact/Contact.jsx
@@ -18,7 +18,7 @@ const Contact = () => {
     e.preventDefault();
     setLoading(true);  
     try {
-      const response = await axios.post("https://my-portfolio-7tse.onrender.com/contact", formData);
+      const response = await axios.post("https://portfolio-hln4.onrender.com/contact", formData);
       console.log(response.data); 
       
       setFormData({
